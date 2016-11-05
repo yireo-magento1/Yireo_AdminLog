8673173ccb21bef54185829729907d8d09a3e9c8
Jisse Reitsma <info@yireo.com>
2016-11-05 07:26:12 +0100

    Pending changes

--- source/app/code/community/Yireo/AdminLog/Helper/Data.php
+++ source/app/code/community/Yireo/AdminLog/Helper/Data.php
@@ -3,10 +3,10 @@
  * Yireo AdminLog for Magento
  *
  * @package     Yireo_AdminLog
- * @author      Yireo (http://www.yireo.com/)
- * @copyright   Copyright 2015 Yireo (http://www.yireo.com/)
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2015 Yireo (https://www.yireo.com/)
  * @license     Open Source License (OSL v3)
- * @link        http://www.yireo.com/
+ * @link        https://www.yireo.com/
  */
 
 class Yireo_AdminLog_Helper_Data extends Mage_Core_Helper_Abstract
